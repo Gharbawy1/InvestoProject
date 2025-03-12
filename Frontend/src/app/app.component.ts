cd7112965b31ac5ba11435f2f4359f9373da9553
Ziad Mohammed <00ziadmohammed606@gmail.com>
2025-03-12 03:10:22 +0200

    Added Header, Footer and adjusted the Routes

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Components/header/header.component';
 import { LoginFormComponent } from './Components/login-form/login-form.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginFormComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginFormComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
+import { LoginFormComponent } from './Components/login-form/login-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'LandingPage', component: LandingPageComponent},
+    {path: 'login', component: LoginFormComponent},
+    {path: '', redirectTo: 'LandingPage', pathMatch: 'full'},
+];
