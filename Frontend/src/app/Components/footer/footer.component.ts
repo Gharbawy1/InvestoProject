220dc43a4412759d799354371c53f83a35c72c22
Ziad Mohammed <00ziadmohammed606@gmail.com>
2025-03-16 02:10:27 +0200

    edited the footer

--- Frontend/src/app/Components/footer/footer.component.ts
+++ Frontend/src/app/Components/footer/footer.component.ts
@@ -1,14 +1,28 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
-
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope, faPhone, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports:[CommonModule],
+  imports:[CommonModule, FaIconComponent,FontAwesomeModule ],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  faFacebook = faFacebook;
+  faTwitter = faTwitter;
+  faInstagram = faInstagram;
+  faLinkedin = faLinkedin;
+
+  // Contact Icons
+  faEnvelope = faEnvelope;
+  faPhone = faPhone;
+  faMapMarker = faMapMarkerAlt;
+  
   @Input() companyName: string = 'Investment Platform';
   @Input() companyDescription: string = 'A comprehensive platform connecting investors with business owners.';
   @Input() contactEmail: string = 'contact@investmentplatform.com';
@@ -40,4 +54,6 @@ export class FooterComponent {
       ]
     }
   ];
+  
+
 }
--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { ProjectFilterComponent } from './Components/project-filter/project-filt
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginFormComponent, FooterComponent, ProjectFilterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
