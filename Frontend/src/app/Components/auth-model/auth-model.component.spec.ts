febcb256a08038619f47604ed5f305cfd05c75d4
Ziad Mohammed <00ziadmohammed606@gmail.com>
2025-03-19 22:42:17 +0200

    edited the register from and the auth-model

--- Frontend/src/app/Components/auth-model/auth-model.component.spec.ts
+++ Frontend/src/app/Components/auth-model/auth-model.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AuthModalComponent } from './auth-modal.component';
+import { AuthModelComponent } from './auth-model.component';
 
 describe('AuthModalComponent', () => {
-  let component: AuthModalComponent;
-  let fixture: ComponentFixture<AuthModalComponent>;
+  let component: AuthModelComponent;
+  let fixture: ComponentFixture<AuthModelComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AuthModalComponent]
+      imports: [AuthModelComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(AuthModalComponent);
+    fixture = TestBed.createComponent(AuthModelComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './Components/landing-page/landing-page.component';
-import { AuthModalComponent } from './Components/auth-modal/auth-modal.component';
+import { AuthModelComponent } from './Components/auth-model/auth-model.component';
 import { ProjectCardComponent } from './Components/project-card/project-card.component';
 
 export const routes: Routes = [
     {path: 'LandingPage', component: LandingPageComponent},
-    {path: 'auth', component: AuthModalComponent},
+    {path: 'auth', component: AuthModelComponent},
     {path: 'projectCard', component: ProjectCardComponent},
     {path: '', redirectTo: 'LandingPage', pathMatch: 'full'},
 ];
