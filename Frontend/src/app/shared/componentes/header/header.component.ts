792f341a968df62cccff978470d9aee0ea3a5597
Ziad Mohammed <00ziadmohammed606@gmail.com>
2025-04-12 13:04:37 +0200

    edited the header

--- Frontend/src/app/shared/componentes/header/header.component.ts
+++ Frontend/src/app/shared/componentes/header/header.component.ts
@@ -1,31 +1,32 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  @Input() isLoggedIn: boolean = true;
-  @Input() userName: string = 'John Doe';
-  @Input() userImageURL: string = 'https://via.placeholder.com/150';
+  @Input() isLoggedIn = false;
+  @Input() userName = 'John Doe';
   @Output() loginClick = new EventEmitter<void>();
   @Output() registerClick = new EventEmitter<void>();
 
   isMenuOpen = false;
-  isDropDownOpen = false;
-  
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
   }
-  
-  toggleDropDown() {
-    this.isDropDownOpen = !this.isDropDownOpen;
-  }
-  
 }
