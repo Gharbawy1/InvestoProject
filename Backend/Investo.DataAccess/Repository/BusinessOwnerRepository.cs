abdcec5a16e08e01a02ca33b94dd11f4cd4f7812
Donia <doniashaban723@gmail.com>
2025-04-24 21:39:49 +0200

    added Business Owner Repository and its interface to make sure BO exists or not and that is Id is a valid Id

---
+++ Backend/Investo.DataAccess/Repository/BusinessOwnerRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Investo.DataAccess.Services.BusinessOwners;
+using Microsoft.EntityFrameworkCore;
+using Investo.Entities.IRepository;
+using Investo.DataAccess.ApplicationContext;
+
+namespace Investo.DataAccess.Repository
+{   
+    public class BusinessOwnerRepository : IBusinessOwnerRepository
+    {
+        private readonly CoreEntitiesDbContext _context;
+        public BusinessOwnerRepository(CoreEntitiesDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<bool> ExistsAsync(string ownerId)
+        {
+            return await _context.BusinessOwners.AnyAsync(o => o.Id == ownerId);
+        }
+    }
+}
