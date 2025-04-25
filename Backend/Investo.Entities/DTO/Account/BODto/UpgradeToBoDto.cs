4240795de51afe177a97732d0464cbea70fe7b05
Ahmed Elghrabawy <ahmedelgharbawy28@gmailcom>
2025-04-25 21:51:55 +0300

    add upgradeUserRole Dtos

---
+++ Backend/Investo.Entities/DTO/Account/BODto/UpgradeToBoDto.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investo.Entities.DTO.Account.BODto
+{
+    public class UpgradeToBoDto
+    {
+        public string NationalID { get; set; }
+        public IFormFile NationalIDImageFrontURL { get; set; }
+        public IFormFile NationalIDImageBackURL { get; set; }
+        public IFormFile ProfilePictureURL { get; set; }
+    }
+}
