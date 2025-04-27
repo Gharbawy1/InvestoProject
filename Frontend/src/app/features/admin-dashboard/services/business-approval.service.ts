6a01c1fe9739f0ef433db23e54b660358cef220e
Ahmedabali <mido57305@gmail.com>
2025-04-27 21:10:19 +0300

    business services

--- Frontend/src/app/features/admin-dashboard/services/business-approval.service.ts
+++ Frontend/src/app/features/admin-dashboard/services/business-approval.service.ts
@@ -9,14 +9,15 @@ import { IBusinessProfile } from '../interfaces/IBusinessProfile';
 })
 export class BusinessApprovalService {
   private apiUrl = `${environment.apiBase}/businessProfiles`;
-  constructor(private http : HttpClient) { }
-  
-  getProjects():Observable<IBusinessProfile[]>{
+
+  constructor(private http: HttpClient) {}
+
+  getProjects(): Observable<IBusinessProfile[]> {
     return this.http.get<IBusinessProfile[]>(this.apiUrl);
   }
-  
-  updateProjectStatus(projectId: string, status: 'approved' | 'rejected'|'pending'): Observable<any> {
-    return this.http.patch(this.apiUrl, { projectId, status });
+
+  updateProjectStatus(projectId: string, status: 'approved' | 'rejected' | 'pending'): Observable<any> {
+    return this.http.patch(`${this.apiUrl}/${projectId}`, { status });
   }
   
 }
