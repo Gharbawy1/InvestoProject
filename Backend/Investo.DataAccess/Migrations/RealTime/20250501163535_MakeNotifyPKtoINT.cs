52c7507c10185f0db728fde0c8841a5660042323
Ahmed Elghrabawy <ahmedelgharbawy28@gmailcom>
2025-05-03 04:31:14 +0300

    no added for notifications just add automitclly ignored files

---
+++ Backend/Investo.DataAccess/Migrations/RealTime/20250501163535_MakeNotifyPKtoINT.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Investo.DataAccess.Migrations.RealTime
+{
+    /// <inheritdoc />
+    public partial class MakeNotifyPKtoINT : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+               name: "PK_Notifications",
+               schema: "RealTime",
+               table: "Notifications");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                schema: "RealTime",
+                table: "Notifications");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                schema: "RealTime",
+                table: "Notifications",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Notifications",
+                schema: "RealTime",
+                table: "Notifications",
+                column: "Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IssuerId",
+                schema: "RealTime",
+                table: "Notifications",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IssuerId",
+                schema: "RealTime",
+                table: "Notifications");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                schema: "RealTime",
+                table: "Notifications",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+        }
+    }
+}
---
+++ Backend/Investo.Entities/IRepository/INotificationRepository.cs
@@ -0,0 +1,14 @@
+﻿using Investo.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Investo.Entities.IRepository
+{
+    public interface INotificationRepository
+    {
+        Task SaveAsync(Notification notification);
+    }
+}
