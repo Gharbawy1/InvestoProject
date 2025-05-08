63f0afe4077d86f21dd45e710b1154034c4325d7
AhmedMyoussif <ahmedhamouda18hhh@gmail.com>
2025-05-08 19:27:38 +0300

    Remove id prop from UpdateCategoryDto as it is auto increment in database

--- Backend/Investo.Entities/DTO/Category/UpdateCategoryDTO.cs
+++ Backend/Investo.Entities/DTO/Category/UpdateCategoryDTO.cs
@@ -9,9 +9,7 @@ namespace Investo.Entities.DTO.Category
 {
     public class UpdateCategoryDto
     {
-        [Range(1, byte.MaxValue, ErrorMessage = "Id must be greater than 0")]
-        public byte Id { get; set; }
-
+        
         [MaxLength(100, ErrorMessage = "Name can't exceed 100 characters")]
         [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
